9f4f1587c1373cd0d8b770d68b7ac35364d8c519
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-01-30 23:41:27 +0530

    Add: Flipping coin multiple times and count outcomes to get winner

--- FlipCoinSimulationProblem.sh
+++ FlipCoinSimulationProblem.sh
@@ -1,10 +1,28 @@
 echo "Welcome to Flip Coin Simulation Problem"
 
-flip=$(( RANDOM%2 ))
+read -p "Enter the nuber of times to toss the coin: " x
+headCount=0
+tailCount=0
 
-if [ $flip -eq 0 ]
+for(( i = 1; i <= x; i++ ))
+do
+	flip=$(( RANDOM%2 ))
+	if(( flip == 0 ))
+	then
+        	echo "Flip number $i - Head"
+        	(( headCount++ ))
+	else
+        	echo "Flip number $i - Tail"
+        	(( tailCount++ ))
+	fi
+done
+echo "The Head count is $headCount and Tail Count is $tailCount"
+if(( headCount > tailCount ))
 then
-    echo "Heads"
+    echo "The winner is Heads"
+elif(( tailCount > headCount ))
+then
+    echo "The winner is Tails"
 else
-    echo "Tails"
+    echo "There is no winner. It is a tie"
 fi
